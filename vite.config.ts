ef07551eec7d155088a5ceaf1bba53c7c6e24d5c
Thyago Machado Cassimiro <t.cassimiro@sidi.org.br>
2023-10-16 15:57:23 -0300

    adjusting package & vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import path, { resolve } from 'path';
+
+import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), libInjectCss(), dts()],
   resolve: {
     alias: [
       {
@@ -12,5 +15,19 @@ export default defineConfig({
         replacement: path.join(__dirname, './src/components'),
       },
     ],
+  },
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, 'src/index.tsx'),
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      },
+    },
   },
 });
